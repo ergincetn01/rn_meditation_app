bf97498ef718cb164b9549bb67ea1b1192c76204
ergincetin-at-058264215011 <ergincetn01@gmail.com>
2025-03-14 00:11:15 +0300

    tabs layout added with 2 screens

--- app/(tabs)/nature-meditate.tsx
+++ app/(tabs)/nature-meditate.tsx
@@ -1,10 +1,57 @@
-import { View, Text } from "react-native"
+import { View, Text, FlatList, Pressable, ImageBackground } from "react-native"
 import React from "react"
+import AppGradient from "@/components/AppGradient"
+import { StatusBar } from "expo-status-bar"
+import { MEDITATION_DATA } from "@/constants/MeditationData"
+import MEDITATION_IMAGES from "@/constants/meditation-images"
+import { LinearGradient } from "expo-linear-gradient"
 
 const NatureMeditate = () => {
 	return (
-		<View>
-			<Text>nature-meditate</Text>
+		<View className="flex-1">
+			<AppGradient colors={["#161b2e", "#0a4d4a", "#766e67"]}>
+				<View className="mb-6">
+					<Text className="text-gray-200 mb-3 text-4xl font-bold text-left">
+						Welcome, Ergin
+					</Text>
+					<Text className="text-left text-indigo-100 text-xl font-medium">
+						Start your meditation practice
+					</Text>
+				</View>
+				<View>
+					<FlatList
+						className="mb-20"
+						data={MEDITATION_DATA}
+						renderItem={({ item }) => (
+							<Pressable
+								className="h-48 my-3 rounded-md overflow-hidden"
+								onPress={() => console.log("pressed")}
+							>
+								<ImageBackground
+									source={MEDITATION_IMAGES[item.id - 1]}
+									resizeMode="cover"
+									className="flex-1 justify-center rounded-lg"
+								>
+									<LinearGradient
+										colors={[
+											"transparent",
+											"rgba(0,0,0, 0.8)",
+										]}
+										className="flex-1 justify-center items-center"
+									>
+										<Text className="text-gray-100 text-3xl text-center font-bold">
+											{item.title}
+										</Text>
+									</LinearGradient>
+								</ImageBackground>
+							</Pressable>
+						)}
+						showsVerticalScrollIndicator
+						keyExtractor={(item) => item.id.toString()}
+					/>
+				</View>
+			</AppGradient>
+			<StatusBar style="light" />
 		</View>
 	)
 }
