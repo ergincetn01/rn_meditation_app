8d62bb580a65b781fbcd9ba421bd3123b975c61f
ergincetin-at-058264215011 <ergincetn01@gmail.com>
2025-03-14 00:09:55 +0300

    app layout updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
 import { View, Text } from "react-native"
 import React from "react"
-import { Slot } from "expo-router"
+import { Slot, Stack } from "expo-router"
 
 const RootLayout = () => {
-	return <Slot />
+	return (
+		<Stack>
+			<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+			<Stack.Screen name="index" options={{ headerShown: false }} />
+		</Stack>
+	)
 }
 
 export default RootLayout
