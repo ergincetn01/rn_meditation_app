d5d64434d9b15eb1044bd222e3d2b613737341fb
ergincetin-at-058264215011 <ergincetn01@gmail.com>
2025-03-15 23:54:04 +0300

    affirmations layout updated

--- app/(tabs)/affirmations/_layout.tsx
+++ app/(tabs)/affirmations/_layout.tsx
@@ -1,11 +1,14 @@
 import { View, Text } from "react-native"
 import React from "react"
+import { Stack } from "expo-router"
 
 const AffirmationsLayout = () => {
 	return (
-		<View>
-			<Text>AffirmationsLayout</Text>
-		</View>
+		<Stack>
+			<Stack.Screen name="index" options={{ headerShown: false }} />
+			<Stack.Screen name="[itemId]" options={{ headerShown: false }} />
+			{/* <Stack.Screen name="meditate" options={{ headerShown: false }} /> */}
+		</Stack>
 	)
 }
 
